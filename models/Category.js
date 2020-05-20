f900a2cb11d4cb9f34c61c1642ba129dec85a3bf
AUB <hammouch.ayoub@yahoo.com>
2020-05-20 17:50:51 +0000

    First commit

---
+++ models/Category.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+//required fields for the category
+var CategorySchema = new mongoose.Schema({
+  id: String,
+  catName: String,
+  catDesc: String,
+  catImgUrl: String,
+  catContent: String,
+  updated: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Category', CategorySchema);
